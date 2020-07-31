7d747296c0d928fd583b05381525a0d01685ed6a
Suthishan <32566422+suthishan@users.noreply.github.com>
2020-07-31 17:13:44 +0530

    Initial commit

---
+++ src/app/sidebar/sidebar-routes.config.ts
@@ -0,0 +1,5 @@
+export const ROUTES = [
+  { path: '/dashboard', title: 'Dashboard', icon: 'dashboard', children: null },
+  { path: 'profile', title: 'User Profile', icon: 'person', children: null },
+
+];
